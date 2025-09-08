0f4db2c7ee2e1404e4a8a465efd1faed566bd70f
PJColombo <paulo.colombo@pm.me>
2025-09-08 19:33:34 +0200

    fix: increase reqwest client request timeout

--- src/context.rs
+++ src/context.rs
@@ -66,7 +66,7 @@ impl Context {
         let exp_backoff = Some(ExponentialBackoffBuilder::default().build());
 
         let client = reqwest::Client::builder()
-            .timeout(Duration::from_secs(8))
+            .timeout(Duration::from_secs(16))
             .build()?;
         let provider = ProviderBuilder::new()
             .network::<Ethereum>()
