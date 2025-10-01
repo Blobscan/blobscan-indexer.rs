a999fb7074b897fa947b4f5371a13ef2d0012049
Pablo Castellano <pablo@anche.no>
2025-10-01 13:41:43 +0200

    docs: show usage parameters and improve wording (#96)

--- src/args.rs
+++ src/args.rs
@@ -44,7 +44,7 @@ impl NumThreads {
     }
 }
 
-/// Blobscan's indexer for the EIP-4844 upgrade.
+/// Blobscan's indexer for blob transactions (EIP-4844).
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 pub struct Args {
@@ -68,7 +68,7 @@ pub struct Args {
     #[arg(short = 'c', long, action = ArgAction::SetTrue)]
     pub disable_sync_checkpoint_save: bool,
 
-    /// Disable historical synchronization
+    /// Disable backfill indexing thread
     #[arg(short = 'd', long, action = ArgAction::SetTrue)]
     pub disable_sync_historical: bool,
 }
